485d9de3f47a87798878773a72624491a05547d5
feng-xian <2218050479@qq.com>
2021-03-21 15:17:00 +0800

    网关

---
+++ hello-spring-cloud-gateway/src/main/java/com/funtl/hello/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,22 @@
+package com.funtl.hello.spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author : YeLei
+ * @Date : 2021/3/15 20:33
+ * @Version : 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}

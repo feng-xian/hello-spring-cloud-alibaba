2bd174085812a9eac6817d9bcf37132318b240af
feng-xian <2218050479@qq.com>
2021-03-21 15:17:25 +0800

    服务提供者

--- hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,6 +1,9 @@
 package com.funtl.hello.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.ConfigurablePropertyAccessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +21,14 @@ public class NacosProviderController {
     @Value("${server.port}")
     private String port;
 
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
+
     @GetMapping("/nacosProvider")
     public String testNacosProvider(String message){
-        return "hello nacos your name is " + message + "and I am from "+port;
+        String name = configurableApplicationContext.getEnvironment().getProperty("user.name");
+        return "hello nacos your name is " + message + " and I am from " +
+                ""+port+" and My name is "+ name;
     }
 
 
